bfaf2498d70259bef6b0e1335b9cc05e38a84a2b
alter-s1 <69759292+alter-s1@users.noreply.github.com>
2021-01-30 15:56:44 +0200

    Done

--- parsing-posts/main.go
+++ parsing-posts/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func main() {
-	resp, _ := http.Get("https://jsonplaceholder.typicode.com/posts")
-	body, _ := ioutil.ReadAll(resp.Body)
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(string(body))
 	resp.Body.Close()
 }
